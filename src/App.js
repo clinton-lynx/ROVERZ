d8b5775380a1fd557eb5cf206d47a2ecc884237a
Anthony Udeh Chinaza <anthonyudehchinaza@gmail.com>
2022-10-10 06:37:12 -0700

    scss changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import './assets/styles/main.scss'
+import Header from './components/Header/Header'
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+       <Header />
     </div>
   );
 }
